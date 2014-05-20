3de8edf59d99080c15b449c668ee2a6a68b87745
Takashi Hashimoto <t.hasmot@gmail.com>
2014-05-20 18:18:45 +0900

    20140520-2

--- insert_users.rb
+++ insert_users.rb
@@ -3,13 +3,18 @@ require 'mongo'
 
 client = Mongo::MongoClient.new
 db = client.db('gplus')
+collection_names = db.collection_names
 
 while line = gets
     item = JSON.parse(line)
     col_name = item['actor']['id']
 
-    if !db.collection_names.include?(col_name)
+    puts col_name
+    puts collection_names
+
+    if !collection_names.include?(col_name)
     	db.create_collection(col_name)
+        collection_names.push(col_name)
     end
 
     col = db.collection(col_name)
